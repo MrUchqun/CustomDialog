5bdc3d7324a10f498c08961678c43b671181f07f
Uchqun Azimboyev <79976346+MrUchqun@users.noreply.github.com>
2022-02-21 18:23:57 +0500

    add custom toast

--- app/src/main/java/com/example/customdialog/ExitDialog.kt
+++ app/src/main/java/com/example/customdialog/ExitDialog.kt
@@ -1,11 +1,14 @@
 package com.example.customdialog
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 
 class ExitDialog : DialogFragment() {
--- app/src/main/java/com/example/customdialog/MainActivity.kt
+++ app/src/main/java/com/example/customdialog/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.customdialog
 
+import android.app.Activity
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         val dialog = ExitDialog()
         dialog.show(supportFragmentManager, "ExitDialog")
 
+        Toast(this).showCustomToast("Ok",this)
+
         dialog.doneClick = {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
@@ -23,5 +29,23 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
 
+    }
+}
+
+fun Toast.showCustomToast(message: String, activity: Activity) {
+
+    val layout = activity.layoutInflater.inflate(
+        R.layout.custom_dialog,
+        activity.findViewById(R.id.custom),
+    )
+
+    val toastMessage = layout.findViewById<TextView>(R.id.cancel_txt)
+    toastMessage.text = message
+
+    this.apply {
+        setGravity(Gravity.BOTTOM,0,200)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
     }
 }
